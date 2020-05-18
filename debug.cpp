7f794a7edadfcb6d07ab058ed0dfb5080f7bcb91
Li-Hung Chen <badstyle319@hotmail.com>
2020-05-18 23:26:34 +0800

    1. rearrange folders

---
+++ debug.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+#ifdef DBG
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+    
+
+    
+#ifdef DBG
+	fclose(stdin);
+	fclose(stdout);
+#endif
+
+	return 0;
+}
