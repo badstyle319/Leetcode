7d283573106db2e0688eea808100635ad3890dad
Luis Chen <luis_chen@raytrex.com>
2022-04-13 22:51:00 +0800

    solution optimized

--- 0000/00002. Add Two Numbers.cpp
+++ 0000/00002. Add Two Numbers.cpp
@@ -45,50 +45,28 @@ public:
         int carry = 0;
         return func(l1, l2, carry);
         
-//         // iterative
-//         ListNode *ans = NULL, *it = NULL;
-//         int carry = 0;
+        /* // iterative       
+		ListNode *pre = new ListNode();
+        ListNode *result = pre;
+        int carry = 0;
         
-//         while(l1 || l2)
-//         {
-//             ListNode* newNode = new ListNode(carry);
-//             if(l1)
-//             {
-//                 newNode->val += l1->val;
-//                 l1 = l1->next;
-//             }
-//             if(l2)
-//             {
-//                 newNode->val += l2->val;
-//                 l2 = l2->next;
-//             }
-//             carry = newNode->val / 10;
-//             newNode->val %= 10;
-//             if(!it)
-//             {
-//                 it = newNode;
-//                 ans = it;
-//             }
-//             else
-//             {
-//                 it->next = newNode;
-//                 it = it->next;
-//             }
-//         }
-//         if(carry)
-//         {
-//             if(!it)
-//             {
-//                 it = new ListNode(carry);
-//                 ans = it;
-//             }
-//             else
-//             {
-//                 it->next = new ListNode(carry);
-//                 it = it->next;
-//             }
-//         }
+        while(l1 || l2 || carry)
+        {
+            if(l1)
+            {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            if(l2)
+            {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            pre->next = new ListNode(carry % 10);
+            pre = pre->next;
+            carry /= 10;
+        } */
         
-//         return ans;
+        return result->next;
     }
 };
