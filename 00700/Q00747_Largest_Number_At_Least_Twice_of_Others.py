2bbccd404eaf3574da16bacd1b87f5bf3130aa8b
Luis Chen <luis_chen@raytrex.com>
2022-07-14 15:24:50 +0800

    feat: solution added

---
+++ 00700/Q00747_Largest_Number_At_Least_Twice_of_Others.py
@@ -0,0 +1,27 @@
+from typing import List
+from unittest import TestCase
+
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        maximum = max(nums)
+        for num in nums:
+            if num != maximum and maximum < 2 * num:
+                return -1
+        return nums.index(maximum)
+
+
+class TestResult(TestCase):
+    def run_test(self, data, expect):
+        result = Solution().dominantIndex(data)
+        self.assertEqual(result, expect)
+
+    def test_case1(self):
+        data = [3, 6, 1, 0]
+        expect = 1
+        self.run_test(data, expect)
+
+    def test_case2(self):
+        data = [1, 2, 3, 4]
+        expect = -1
+        self.run_test(data, expect)
