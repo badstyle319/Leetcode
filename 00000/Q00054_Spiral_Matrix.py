908494d4ffe98f7c9f015e1d5bcb4f101fdc7ec1
Luis Chen <badstyle319@hotmail.com>
2022-08-27 17:50:43 +0800

    style: warnings resolved

--- 00000/Q00054_Spiral_Matrix.py
+++ 00000/Q00054_Spiral_Matrix.py
@@ -31,13 +31,13 @@ class TestResult(unittest.TestCase):
         self.assertEqual(result, expect)
 
     def test_case1(self):
-        data = [[1, 2, 3],[4,5,6],[7,8,9]]
-        expect = [1,2,3,6,9,8,7,4,5]
+        data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        expect = [1, 2, 3, 6, 9, 8, 7, 4, 5]
         self.run_test(data, expect)
 
     def test_case2(self):
-        data = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-        expect = [1,2,3,4,8,12,11,10,9,5,6,7]
+        data = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+        expect = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
         self.run_test(data, expect)
 
 
