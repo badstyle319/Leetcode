1575bd3e3b54223ac4225792f8b43248782044be
Luis Chen <luis_chen@raytrex.com>
2023-01-09 22:00:55 +0800

    feat: linked list function added

---
+++ common.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def create_linked_list(data):
+    if not data:
+        return None
+
+    head = ListNode(data[0])
+    cur = head
+    for val in data[1:]:
+        cur.next = ListNode(val)
+        cur = cur.next
+
+    return head
